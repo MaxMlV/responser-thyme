ac12e94e2bb93db2489e0948dd83291ed6baa519
MaxMlV <max.mlv.k@gmail.com>
2021-05-01 23:29:59 +0300

    finished 'edit profile' feature

--- src/main/resources/static/js/edit_modal.js
+++
@@ -1,9 +0,0 @@
-
-$('#updateUserModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget)
-    var firstName = button.data('first-name')
-    var lastName = button.data('last-name')
-    var modal = $(this)
-    modal.find('.modal-body #floatingFirstName').val(firstName)
-    modal.find('.modal-body #floatingLastName').val(lastName)
-})
